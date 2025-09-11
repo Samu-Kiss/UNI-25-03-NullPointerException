413edc1b6543cb6fa3938d8c381fb2249779f871
Samuel Pico <sa.pico@javeriana.edu.co>
2025-09-11 15:32:30 -0500

    [Fix]: Pardon, je suis desolé

--- Pontiland/src/test/java/com/NullPtr/Pontiland/view/GameApplicationTest.java
+++ Pontiland/src/test/java/com/NullPtr/Pontiland/view/GameApplicationTest.java
@@ -1,19 +1,3 @@
 package com.NullPtr.Pontiland.view;
-
-import com.jme3.app.SimpleApplication;
-import com.jme3.light.AmbientLight;
-import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
-import com.jme3.system.AppSettings;
-import com.jme3.texture.Texture;
-import com.jme3.util.SkyFactory;
-
-import java.awt.*;
-
 public class GameApplicationTest{
 }
