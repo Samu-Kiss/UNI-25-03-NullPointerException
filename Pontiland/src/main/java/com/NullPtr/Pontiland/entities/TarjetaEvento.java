f29b43bae8da642f92e87e039c5ceacc54b7d8e6
Nicolas Torres Roa <178304045+N-Torresito@users.noreply.github.com>
2025-09-23 15:29:43 -0500

    Feat: Update Jugador class to include Estado enum for player state management to future-proof for any other Estado that could surge during development

--- Pontiland/src/main/java/com/NullPtr/Pontiland/entities/TarjetaEvento.java
+++ Pontiland/src/main/java/com/NullPtr/Pontiland/entities/TarjetaEvento.java
@@ -1,32 +1,21 @@
 package com.NullPtr.Pontiland.entities;
 
 /**
- * Enum que representa las posibles acciones que una tarjeta de evento puede tener
- */
-enum Accion {
-    bajarNivel, aumentarNivel, propiedadNivel1, propiedadNivel5,
-    CobroAJugador,
-    AbonoAJugador50, AbonoAJugador100, AbonoAJugador200
-}
-
-/**
  * Clase que representa una tarjeta de evento en el juego.
  * Cada tarjeta tiene un nombre, una descripción y una acción asociada
  */
 public class TarjetaEvento {
     private String nombre = null;
     private String descripcion = null;
-    private Accion accion;
 
     /**
      * Constructor de la clase TarjetaEvento
      * @param nombre Nombre de la tarjeta
      * @param descripcion Descripción de la tarjeta
-     * @param accion Acción asociada a la tarjeta
      * @throws IllegalArgumentException si algún parámetro es nulo o si el nombre o la descripción están vacíos
      */
-    public TarjetaEvento(String nombre, String descripcion, Accion accion) {
-        if (nombre == null || descripcion == null || accion == null) {
+    public TarjetaEvento(String nombre, String descripcion) {
+        if (nombre == null || descripcion == null) {
             throw new IllegalArgumentException("Ningún parámetro puede ser nulo");
         }
         if (nombre.isEmpty() || descripcion.isEmpty()) {
@@ -34,7 +23,6 @@ public class TarjetaEvento {
         }
         this.nombre = nombre;
         this.descripcion = descripcion;
-        this.accion = accion;
     }
 
 }
