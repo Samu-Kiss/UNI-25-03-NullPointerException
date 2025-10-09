9c73e3af172c10b1aff8a31f58b1746f7c637f6d
Samuel Pico <sa.pico@javeriana.edu.co>
2025-10-09 11:25:47 -0500

    [Feat]: Implement properties-based configuration for game save location

--- Pontiland/src/main/java/com/NullPtr/Pontiland/view/MenuCarga.java
+++ Pontiland/src/main/java/com/NullPtr/Pontiland/view/MenuCarga.java
@@ -194,7 +194,8 @@ public class MenuCarga extends AbstractAppState {
   }
 
   private void onBack() {
-    // TODO: Review if cleanup and detaching should occur after goToMainMenu(), or if additional error handling is needed.
+    // TODO: Review if cleanup and detaching should occur after goToMainMenu(), or if additional
+    // error handling is needed.
     try {
       if (actions != null) actions.goToMainMenu();
     } finally {
